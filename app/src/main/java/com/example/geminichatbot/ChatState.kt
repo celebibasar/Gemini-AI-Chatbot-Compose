642d7d8c3f554209c931a890e1070e0a8f934e7b
celebibasar <basarcelebi02@gmail.com>
2024-03-12 18:46:42 +0300

    New update

---
+++ app/src/main/java/com/example/geminichatbot/ChatState.kt
@@ -0,0 +1,10 @@
+package com.example.geminichatbot
+
+import android.graphics.Bitmap
+import com.example.geminichatbot.data.Chat
+
+data class ChatState(
+    val chatList: MutableList<Chat> = mutableListOf(),
+    val prompt: String = "",
+    val bitmap: Bitmap? = null
+)
---
+++ app/src/main/java/com/example/geminichatbot/data/Chat.kt
@@ -0,0 +1,9 @@
+package com.example.geminichatbot.data
+
+import android.graphics.Bitmap
+
+class Chat (
+    val prompt: String,
+    val bitmap: Bitmap?,
+    val isFromUser: Boolean
+)
